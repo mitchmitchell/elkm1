3e74d0879f1d40a3b50988b66022e2004efdc83b
Glenn Waters <glenn@watrs.ca>
2025-01-15 22:10:42 -0500

    Formatting.

--- test/util.py
+++ test/util.py
@@ -9,7 +9,7 @@ def rx_msg(msg_code: str, msg: str, notifier: Notifier, zeros="00"):
     Create a Elk received message, pass it to decode, and
     invoke the notifiers which will update the base element.
     """
-    data = f"{len(msg)+len(zeros)+4:02X}{msg_code}{msg}{zeros}"
+    data = f"{len(msg) + len(zeros) + 4:02X}{msg_code}{msg}{zeros}"
     cksum = (256 - reduce(lambda x, y: x + y, map(ord, data))) % 256
     decoded = decode(f"{data}{cksum:02X}")
     if decoded:
